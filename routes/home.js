e840f931cf8c33f6a42e226f7e88c7ab2ba11464
sarthakagsa <sarthakag.sa@gmail.com>
2021-01-11 20:53:31 +0530

    Removed signup link

--- routes/home.js
+++ routes/home.js
@@ -6,9 +6,9 @@ router.get('/login',(req,res)=>{
     res.render('login')
 })
 
-router.get('/signup',(req,res)=>{
-    res.render('signup')
-})
+// router.get('/signup',(req,res)=>{
+//     res.render('signup')
+// })
 
 router.get('/home/:token',auth,(req,res)=>{
     res.render('home',{user:req.user,token: req.token})
