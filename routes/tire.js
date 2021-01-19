945061b7d34c3ec5169ac2cdd24458d2debf83c2
sarthakagsa <sarthakag.sa@gmail.com>
2021-01-19 17:49:09 +0530

    Added Search Option in View SIngle LR

--- routes/tire.js
+++ routes/tire.js
@@ -25,40 +25,38 @@ router.post('/tire/:id/:token',auth,async(req,res)=>{
     }
 })
 
-// router.patch('/repair/:vechileid/:repairid',auth,async(req,res)=>{
-//     const updates = Object.keys(req.body)
-//     const allowedUpdates =['parts','date','cost','invoice','partyname','km','gst']
-//     const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
-//     if (!isValidOperation) {
-//         return res.status(400).send({ error: 'Invalid Updates!'})       
-//     }
-//     req.body.parts.forEach(element => {
-//         const partsUpdates = Object.keys(element)
-//         const partsAllowedUpdates = ['partname','cost','quantity','hsn','partnumber']
-//         const isValidOperation = partsUpdates.every((update)=>partsAllowedUpdates.includes(update))
-//         if (!isValidOperation) {
-//             return res.status(400).send({ error: 'Invalid Updates!'})       
-//         }
-//     })
-//     try {
-//         const vechile = await Vechile.findOne({_id:req.params.vechileid,owner : req.user._id})        
-        
-//         //const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new : true, runValidators : true})
-//         if (!vechile) {
-//             return res.status(404).send({error : "Vechile is not found"})
-//         }
-
-//         const repair = await Repair.findOne({_id: req.params.repairid,vechileid:req.params.vechileid})
-//         if (!repair) {
-//             return res.status(404).send({error : "Tthe following docs is not found"})
-//         }
-//         updates.every((update)=> repair[update] = req.body[update] )
-//         await repair.save()
-//         res.send(repair)
-//     } catch (e) {
-//         return res.status(404).send(e)
-//     }
-// })
+router.patch('/tire/:vechileid/:tireid/:token',auth,async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates =['parts','date','cost','invoice','partyname','km','gst']
+    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates!'})       
+    }
+    req.body.parts.forEach(element => {
+        const partsUpdates = Object.keys(element)
+        const partsAllowedUpdates = ['partname','cost','quantity','hsn','partnumber']
+        const isValidOperation = partsUpdates.every((update)=>partsAllowedUpdates.includes(update))
+        if (!isValidOperation) {
+            return res.status(400).send({ error: 'Invalid Updates!'})       
+        }
+    })
+    try {
+        const vechile = await Vechile.findOne({_id:req.params.vechileid,owner : req.user._id})
+        //const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new : true, runValidators : true})
+        if (!vechile) {
+            return res.status(404).send({error : "Vechile is not found"})
+        }
+        const tire = await Tire.findOne({_id: req.params.tireid,vechileid:req.params.vechileid})
+        if (!tire) {
+            return res.status(404).send({error : "Tthe following docs is not found"})
+        }
+        updates.every((update)=> tire[update] = req.body[update] )
+        await tire.save()
+        res.send({tire : tire, token : req.token, vechileid : req.params.vechileid})
+    } catch (e) {
+        return res.status(404).send(e)
+    }
+})
 
 router.get('/tire/:vechileid/:tireid/:token',auth,async (req,res)=>{
     try {
